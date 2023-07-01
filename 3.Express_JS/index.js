c8269dee7a15eb95c24f7e947d975d407c1ef44a
vats <vatsalmangukiya9003@gmail.com>
2023-07-01 17:29:01 +0530

    Install EXPRESS

---
+++ 3.Express_JS/index.js
@@ -0,0 +1,12 @@
+const fs=require('fs');
+const express=require('express');
+const server=express();
+
+server.get('/',(req,res)=>{
+       
+});
+
+
+server.listen(8080,()=>{
+       console.log("Server Statred");
+});
